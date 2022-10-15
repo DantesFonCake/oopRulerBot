0489fa309e51d2294fa4602f392f22e1d69e1a06
aripo <aripovsergey3@gmail.com>
2022-10-15 16:30:00 +0500

    раздатчик ролей

--- OopRulerBot/OopRulerBot/DisscordControllers/InfoController.cs
+++ OopRulerBot/OopRulerBot/DisscordControllers/InfoController.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Vostok.Logging.Abstractions;
 
 namespace OopRulerBot.DisscordControllers;
@@ -17,5 +18,24 @@ public class InfoController : ModuleBase<SocketCommandContext>
     public async Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
     {
         await ReplyAsync(echo);
+    }
+
+    [Command("role")]
+    [Summary("Gives a role. Creates one if it doesn't exist now")]
+    public async Task GiveRole([Remainder] [Summary("Role name")] string roleName)
+    {
+        if (Context.User is not IGuildUser guildUser)
+            throw new InvalidOperationException("User is not a guild user");
+        
+        IRole? role = Context.Guild.Roles.SingleOrDefault(x => x.Name == roleName);
+        if (role is null)
+        {
+            log.Info("Role {0} doesn't exist. Creating one", roleName);
+            role = await Context.Guild.CreateRoleAsync(roleName);
+            log.Info("Role {0} created", role.Mention);
+        }
+
+        await guildUser.AddRoleAsync(role);
+        await ReplyAsync($"Gave role {role.Mention} to {guildUser.Mention}");
     }
 }
